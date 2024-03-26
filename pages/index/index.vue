02e6dee0b12974f98669cf93fc4601c287696084
thisCliche <738179359@msn.cn>
2024-03-26 21:36:28 +0800

    tempsubmit

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -10,7 +10,8 @@
 		<view class="model1">
 			<view class="slogan fadeIn animated">基于人工智留学网站基于人工智能的留学网站基于人工智能的留学网站</view>
 			<view class="pictureCom">
-				<view class="pictureComWrap animated fadeInUp-20" :class="'list'+idx" v-for="(item,idx) in model1ComImg">
+				<view class="pictureComWrap animated fadeInUp-20" :class="'list'+idx"
+					v-for="(item,idx) in model1ComImg">
 					<template v-if="idx==2">
 						<img class="img1" :src="item" alt="" />
 						<img class="img2" :src="item" alt="" />
@@ -37,25 +38,64 @@
 				<img :src="bannerUrl" alt="" />
 			</view>
 		</view>
+		<view class="model4">
+			<view class="model4Wrap">
+				<view class="slogan1">大大小小的留学团队都依赖TOBE</view>
+				<view class="slogan2">tobe可以安全扩展，支持学生的DIY操作</view>
+				<view class="btns">
+					<view class="btn deepColor">注册成为TOBE的合作伙伴</view>
+					<view class="btn lightColor">欢迎加入TOBE大家庭</view>
+				</view>
+				<view class="countNumWrap">
+					<view class="countNum">
+						<view class="num">84%</view>
+						<view class="des">认为录取更好了</view>
+					</view>
+					<view class="countNum">
+						<view class="num">84%</view>
+						<view class="des">认为录取更好了</view>
+					</view>
+					<view class="countNum">
+						<view class="num">84%</view>
+						<view class="des">认为录取更好了</view>
+					</view>
+				</view>
+				<view class="littleTag">
+					<view class="tagMono" :class="'icon'+idx" v-for="(item,idx) in tagIcon" :key="idx">
+						<img :src="item.url" alt="" />
+						<span>{{item.num}}</span>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="model5">
+			<view class="slogan">我们的合作伙伴</view>
+			<home-case :homecase="homecase"></home-case>
+		</view>
+		<view class="model6">
+			<view class="adsense">
+				<img :src="bannerUrl" alt="" />
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
 	import bannerUrl from '@image/banner.png';
-	import consulting from '@image/consulting.jpg';
-	import design from '@image/design.jpg';
-	import plate from '@image/plate.jpg';
-	import tech from '@image/tech.jpg';
-	import project from '@image/project.jpg';
+	import {model1ComImg,tagIcon,homecase} from './export.js'
+	import HomeCase from './HomeCase.vue';
 	import {
 		login
 	} from '@/assets/api/homeApi.js'
 	export default {
-		name: 'home',
+		name: 'PagesIndex',
+		components:{HomeCase},
 		data() {
 			return {
+				homecase,
+				tagIcon,
 				bannerUrl,
-				model1ComImg: [consulting, design, plate, tech, project],
+				model1ComImg,
 				banner: [{
 					bannerUrl,
 					id: 1
@@ -86,7 +126,7 @@
 
 	.IndexPage {
 		.banner {
-			&>>>.el-carousel__arrow {
+			&::v-deep .el-carousel__arrow {
 				width: 50px;
 				height: 50px;
 				background-color: rgba(31, 45, 61, 0.5);
@@ -103,16 +143,154 @@
 				object-fit: cover;
 			}
 		}
-
+		.model6{
+			@include ct1400;			
+			.adsense {
+				width: 100%;
+				height: 460px;
+				overflow: hidden;
+				img {
+					width: 100%;
+					height: 100%;
+					object-fit: cover;
+				}
+			}
+		}
+		.model5{
+			@include ct1400;
+			padding: 40px 0;
+			font-weight: 700;
+			.slogan{
+				font-size: 34px;
+				text-align: center;
+			}
+		}
+		.model4{
+			background-color: $bc;
+			padding: 40px 0;
+			.model4Wrap{
+				@include ct1400;
+				position: relative;
+				.littleTag{
+					.tagMono{
+						position: absolute;
+						width: 100px;
+						height: 60px;
+						background-color: $tagBg;
+						border-radius: 8px;
+						@include fj(center);
+						align-items: center;
+						transition: 0.3s all ease-in-out;
+						&:hover{
+							box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+							transform: translate3d(0, -3px, 0px);
+						}
+						img{
+							@include wh(44px,38px);
+							@include mr(8px);
+						}
+						span{
+							font-family: Impact;
+							font-size: 28px;
+						}
+					}
+					.icon0{
+						left: 0px;
+						top: 10px;
+					}
+					.icon1{
+						left: 90px;
+						top: 100px;
+					}
+					.icon2{
+						left: 20px;
+						top: 230px;
+					}
+					.icon3{
+						right: 100px;
+						top: 20px;
+					}
+					.icon4{
+						right: 20px;
+						top: 120px;
+					}
+					.icon5{
+						right: 120px;
+						top: 230px;
+					}
+					
+				}
+				.countNumWrap{
+					@include fj();
+					.countNum{
+						width: 800px;
+						text-align: center;
+						margin: 140px auto 40px;
+						.num{
+							font-family: Impact;
+							font-size: 80px;
+							color: $themeFontColor;
+						}
+						.des{
+							font-family: Impact;
+							font-weight: 700;
+							font-size: 30px;
+							color: $themeFontColor;
+						}
+					}
+				}
+				.slogan1{
+					font-size: 34px;
+					font-weight: 700;
+					margin: 20px auto;
+					text-align: center;
+				}
+				.slogan2{
+					font-size: 24px;
+					font-weight: 700;
+					margin: 40px auto 60px;
+					text-align: center;
+				}
+				.btns{
+					@include fj();
+					width: 600px;
+					margin: 0 auto;
+					.btn{
+						width: 280px;
+						height: 80px;
+						line-height: 80px;
+						text-align: center;
+						border-radius: 10px;
+						font-size: 20px;
+						cursor: pointer;
+						transition: 0.3s all ease-in-out;
+						&:hover{
+							box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+							transform: translate3d(0, -3px, 0px);
+						}
+					}
+					.deepColor{
+						background-color: $themeFontColor;
+						color: #fff;
+					}
+					.lightColor{
+						border:1px solid #333;
+						color: #333;
+					}
+				}
+			}
+		}
 		.model3 {
 			@include ct1400;
 			padding: 40px 0;
-			.adsense{
+
+			.adsense {
 				width: 100%;
 				height: 460px;
 				border-radius: 10px;
 				overflow: hidden;
-				img{
+
+				img {
 					width: 100%;
 					height: 100%;
 					object-fit: cover;
