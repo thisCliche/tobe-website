eda76e2063ef57aa96867482c1c957ee7d05846e
浮華城空 <835036768@qq.com>
2024-04-12 17:33:22 +0800

    个人中心

---
+++ pages/me/index.vue
@@ -0,0 +1,547 @@
+<template>
+	<view class="containerMe">
+		<view class="wap">
+			<view class="wap1">
+				<view class="avatar">
+					<el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+				</view>
+				<view class="des">
+					<view class="name inline">
+						名称
+
+					</view>
+					<view class="inline">
+						ID：xxxxxx
+
+					</view>
+					<view class="inline qianming ">
+						个人签名：xxxxxxccccccccccccccccccccc
+
+					</view>
+					<view class="setting setting1">
+						个人设置
+					</view>
+					<view class="setting setting2">
+						联系TOBE小助手
+					</view>
+
+
+				</view>
+
+				<view class="record">
+					<view class="recordName">
+						最近购买
+
+					</view>
+					<el-scrollbar class="record-list" wrap-style="overflow-x:hidden;">
+
+						<view class="list-item" v-for="(item,index) in 10 " :key="index">
+							<view class="item-left">
+								<img src="https://tupian.qqw21.com/article/UploadPic/2018-6/20186211924257197.jpg"
+									alt="" srcset="" />
+
+							</view>
+
+							<view class="item-right">
+								<view class="text">
+									智能文书-文书测评
+								</view>
+
+								<view class="text">
+									剩余：10次
+								</view>
+
+
+							</view>
+
+
+
+						</view>
+
+
+
+
+
+
+					</el-scrollbar>
+
+				</view>
+
+
+			</view>
+			<view class="wap2">
+				<view class="header">
+					<view class="header-img">
+						<img :src="email" alt="" srcset="" />
+
+					</view>
+					<view class="header-name">
+						我的信箱
+
+					</view>
+
+
+				</view>
+				<view class="message">
+					<text>2445 messages </text>
+					<text style="margin: 0 20px;">2 Unread</text>
+					<img :src="notice" alt="" class="notice" />
+				</view>
+				<view class="content">
+					<el-scrollbar class="content-list" wrap-style="overflow-x:hidden;">
+						<view class="content-item" v-for="(item,index) in 18 " :key="index">
+							<view class="item-left">
+								<view class="tui">
+									问
+								</view>
+							</view>
+							<view class="item-right">
+								<view class="title">
+									推荐信已生成
+								</view>
+								<view class="subtitle">
+									【tobe】推荐信系统已完成您的订单-清华李教授
+
+
+								</view>
+								<view class="contentText">
+									To who it may concern,
+									1111111111111111111111111111111111111
+								</view>
+
+							</view>
+
+						</view>
+
+					</el-scrollbar>
+
+
+
+				</view>
+
+
+
+
+
+
+			</view>
+			<view class="wap3">
+				<view class="header">
+					<view class="header-img">
+						<img :src="heart" alt="" srcset="" />
+
+					</view>
+					<view class="header-name">
+						我的收藏
+
+					</view>
+					
+
+
+				</view>
+				<view class="foloderWap">
+					 <view class="foloderItme" v-for="(item,index) in  folderList" :key="index">
+						 <view class="foloderImg">
+							 <img :src="folder" alt="" srcset="" />
+						 	
+						 </view>
+						 <view class="folderName">
+							 {{item}}
+						 	
+						 </view>
+					 	
+					 </view>
+					
+				</view>
+
+			</view>
+
+		</view>
+
+		<my-foot></my-foot>
+	</view>
+</template>
+
+<script>
+	import email from '@image/email.png';
+	import heart from '@image/heart.png';
+	import notice from '@image/my/notice.png';
+	import folder from '@image/my/folder.png';
+	export default {
+		data() {
+			return {
+				email,
+				notice,
+				heart,
+				folder,
+				folderList:['aaa','bbbb','cccc','dddddd'],
+
+			}
+		},
+		methods: {
+
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.containerMe {
+
+		.wap {
+			@include modelPdOther;
+			@include ct1400;
+			@include fj();
+
+			.wap1 {
+				@include wh(100%, 100%);
+				@include fj(flex-start);
+				flex-direction: column;
+				width: 400px;
+				margin-bottom: 40px;
+
+				.avatar {
+					width: 80px;
+					height: 80px;
+
+					::v-deep .el-avatar {
+
+						width: 80px;
+						height: 80px;
+					}
+
+				}
+
+				.des {
+					margin-top: 40px;
+					display: flex;
+					flex-direction: column;
+
+					.inline {
+						width: 100%;
+						height: 26px;
+						line-height: 26px;
+
+						&.name {
+							color: rgb(0, 0, 0);
+							font-size: 22px;
+							line-height: 26px;
+
+						}
+
+						.qianming {
+							word-wrap: break-word;
+						}
+					}
+
+					.setting {
+						margin-top: 20px;
+						border-radius: 10px;
+						padding: 10px 5px;
+						color: #fff;
+						background: rgb(90, 2, 94);
+						cursor: pointer;
+						text-align: center;
+
+						&.setting1 {
+							width: 20%;
+						}
+
+						&.setting2 {
+							width: 35%;
+						}
+
+					}
+
+
+				}
+
+				.record {
+					display: flex;
+					flex-direction: column;
+					margin-top: 20px;
+
+					.recordName {
+						font-size: 26px;
+						color: #000;
+						font-weight: 700;
+						padding: 20px 0;
+					}
+
+					.record-list {
+
+						box-sizing: border-box;
+						border-radius: 5px;
+						height: 580px;
+
+						.list-item {
+							display: flex;
+							height: 98px;
+							margin: 16px 0;
+							border: 1px solid rgb(202, 202, 202);
+							border-radius: 6px;
+							box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+							padding: 0 16px;
+							align-items: center;
+
+							.item-left {
+								width: 60px;
+								height: 60px;
+								border-radius: 5px;
+								display: flex;
+								justify-content: center;
+								align-items: center;
+
+								img {
+									width: 100%;
+									height: 100%;
+									object-fit: cover;
+								}
+							}
+
+							.item-right {
+								@include fj(space-around);
+								height: 100%;
+								flex-direction: column;
+								padding: 16px;
+								flex: 1;
+
+
+
+							}
+
+
+						}
+
+
+
+					}
+
+
+				}
+
+
+
+
+
+			}
+
+			.wap2 {
+				@include wh(100%, 100%);
+				@include fj(flex-start);
+				flex-direction: column;
+				flex:1;
+				margin: 0 20px 4px 20px;
+			padding-right: 60px;
+
+
+
+				.header {
+					@include fj(flex-start);
+					align-items: center;
+					width: 100%;
+					height: 76px;
+
+					.header-img {
+						width: 40px;
+						height: 56px;
+
+						img {
+							width: 100%;
+							height: 100%;
+							object-fit: cover
+						}
+					}
+
+					.header-name {
+						margin-left: 40px;
+						font-size: 32px;
+						font-weight: 700;
+						color: #000;
+					}
+
+
+
+				}
+
+				.message {
+					display: inline-flex;
+					justify-content: flex-end;
+					align-items: center;
+					padding-right: 60px;
+				}
+
+				.content {
+					width: 100%;
+
+					margin-top: 20px;
+
+					background: rgb(255, 255, 255);
+
+					.content-list {
+						height: 900px;
+
+						.content-item {
+							@include fj(flex-start);
+							width: 100%;
+							height: 100px;
+							padding: 12px 20px;
+							box-sizing: border-box;
+							border-bottom: 0.2px solid rgba(0, 0, 0, 0.15);
+							;
+
+							.item-left {
+								width: 30px;
+								height: 100%;
+								display: flex;
+								justify-content: center;
+								align-items: center;
+
+								.tui {
+									width: 30px;
+									height: 30px;
+									border-radius: 50%;
+									color: #fff;
+									background: rgb(215, 57, 51);
+									display: flex;
+									justify-content: center;
+									align-items: center;
+								}
+
+
+
+							}
+
+							.item-right {
+								display: flex;
+								flex-direction: column;
+								margin-left: 20px;
+
+								.title {
+									color: rgb(0, 0, 0);
+									font-size: 16px;
+									font-weight: 600;
+									line-height: 18px;
+									letter-spacing: 0%;
+									text-align: left;
+
+								}
+
+								.subtitle {
+									color: rgb(0, 0, 0);
+									font-size: 14px;
+									line-height: 16px;
+
+								}
+
+								.contentText {
+									overflow: hidden;
+									text-overflow: ellipsis;
+									display: -webkit-box;
+									// 控制行数
+									-webkit-line-clamp: 2;
+									-webkit-box-orient: vertical;
+									width: 300px;
+									font-size: 12px;
+
+
+								}
+
+
+
+
+
+
+							}
+
+
+
+						}
+
+					}
+
+
+				}
+
+
+			}
+
+
+			.wap3 {
+				@include wh(100%, 100%);
+				@include fj(flex-start);
+				flex-direction: column;
+				width: 320px;
+				margin: 0 20px 4px 20px;
+
+				.header {
+					@include fj(flex-start);
+					align-items: center;
+					width: 100%;
+					height: 76px;
+
+					.header-img {
+						width: 70px;
+						height: 63px;
+
+						img {
+							width: 100%;
+							height: 100%;
+							object-fit: cover
+						}
+					}
+
+					.header-name {
+						margin-left: 40px;
+						font-size: 32px;
+						font-weight: 700;
+						color: #000;
+					}
+
+
+
+				}
+				.foloderWap{
+					width: 100%;
+					display: flex;
+					justify-content: space-between;
+					flex-wrap: wrap;
+					margin-top: 20px;
+					.foloderItme{
+						width: 120px;
+						height: 120px;
+						margin: 25px 40px 10px 0;
+						
+						img {
+							width: 100%;
+							height: 100%;
+							object-fit: cover;
+						}
+						
+					}
+					.folderName{
+						font-size: 24px;
+						color: #000;
+						font-weight: 700;
+						display: flex;
+						justify-content: center;
+						align-items: center;
+						
+					}
+					
+					
+					
+					
+				}
+
+			}
+
+		}
+
+
+
+
+
+
+	}
+</style>
---
+++ pages/resource/school/detail.vue
@@ -0,0 +1,297 @@
+<template>
+	<view class="IndexPage">
+		<view class="banner">
+			<VideoProfile :introduction="introduction" withValue="100%" heightValue="700px" borderRidius="0px" />
+			<view class="star el-icon-star-off" v-if='!isShow'>
+			</view>
+			<view class="star2 el-icon-star-on" v-else>
+			</view>
+			<view class="logo"> </view>
+		</view>
+		<view class="container1">
+			<view class="title"> 人工智能与chatgpt </view>
+			<view class="content">
+				<!-- <rich-text :nodes="content"></rich-text> -->
+				<view class="xiangmu">
+					<view class="title">
+						项目介绍
+
+					</view>
+					<view class="text">
+						文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字
+
+					</view>
+
+				</view>
+				<view class="xiangmu">
+					<view class="title">
+						导师介绍
+
+					</view>
+					<view class="text">
+						文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字文字
+
+					</view>
+
+				</view>
+			</view>
+		</view>
+		<view class="foot">
+			<view class="foot-img">
+				<img src="" alt="" srcset="" />
+			</view>
+			<view class="foot-text"> 入学申请咨询 </view>
+		</view>
+
+		<my-foot></my-foot>
+	</view>
+</template>
+
+<script>
+	import {
+		tagIcon
+	} from "../export.js";
+
+	import UseCase from "../UseCase.vue";
+	import VideoProfile from "@/assets/components/VideoProfile.vue";
+	import HomeCase from "../HomeCase.vue";
+	import {
+		videoApi,
+		bannerApi,
+		configApi,
+		userSide,
+		textConfig,
+		suanFa1,
+		teatcha1,
+	} from "@api/study.js";
+	export default {
+		name: "PagesIndex",
+		components: {
+			UseCase,
+			VideoProfile,
+			HomeCase,
+		},
+		data() {
+			return {
+				isShow: false,
+				introduction: {},
+				content: '<p style="text-indent: 2em;">新华社北京4月7日电&ensp;国务院总理李强4月7日上午在北京人民大会堂会见美国财政部长耶伦。</p><p style="text-indent: 2em;">李强表示，在两国元首战略指引下，当前中美关系出现企稳态势。习近平主席不久前同拜登总统通电话，双方一致同意要加强对话、管控分歧、推进合作，推动中美关系稳定发展。中方希望中美做伙伴而不是对手，相互尊重、和平共处、合作共赢。希望美方同中方更多相向而行，继续落实好两国元首重要共识，让“旧金山愿景”成为“实景”。</p><p style="text-align: center"><img src="./W020240407754781682465_ORIGIN.jpg" data-uploadpic="U020240407754591701123_ORIGIN.jpg" needdownload="true" data-needdownload="true" title="" alt="" style="" picname="1.jpg" oldsrc="W020240407754781682465_ORIGIN.jpg" data-width="800" data-height="602"></p><p label="居中对齐" style="text-align: center; text-indent: 0em;"><span style="color: rgb(0, 0, 255);">4月7日上午，国务院总理李强在北京人民大会堂会见美国财政部长耶伦。新华社记者 燕雁 摄</span></p><p style="text-indent: 2em;">李强指出，中美作为世界前两大经济体，经济利益深度交融，加强经贸合作对双方各自发展和全球经济增长都具有重要意义。双方要加强沟通，共同找到管控分歧、解决分歧的办法，让中美经贸合作稳定、顺畅、高效，为两国企业和人民创造更多实惠，也为世界经济发展和民生改善作出贡献。希望美方与中方一道，坚持公平竞争、开放合作的市场经济基本准则，不把经贸问题泛政治化、泛安全化。要以市场眼光和全球视野，从经济规律出发，客观、辩证看待产能问题。中国的新能源产业发展，将对全球绿色低碳转型作出重要贡献。中方愿同美方在应对气候变化等问题上加强政策协调，共同应对全球性挑战。</p><p style="text-indent: 2em;">耶伦表示，在双方共同努力下，美中关系变得更加稳定。作为全球前两大经济体，美中双方应当负责任地管理双边经济关系。美方赞赏美中经济对话合作取得的进展，不寻求同中方“脱钩”，愿同中方一道落实两国元首旧金山会晤达成的重要共识，坦诚沟通，避免误解，深化交流合作，妥善管控分歧，共同应对紧迫的全球性挑战，推动美中关系稳定发展。</p>',
+			};
+		},
+		onLoad() {
+			this.getData();
+		},
+		methods: {
+			async getData() {
+				let resDataList = await Promise.all([
+					videoApi({
+						type: 4,
+						limit: 1,
+					}),
+					videoApi({
+						type: 5,
+						limit: 2,
+					}),
+
+					videoApi({
+						type: 6,
+						limit: 2,
+					}),
+					videoApi({
+						type: 7,
+						limit: 4,
+					})
+				]);
+				let topVideo = [],
+					cePinginfo = [],
+					howVideoInfo = [],
+
+					usecase = [];
+				resDataList.map((res, idx) => {
+					console.log(res, idx);
+					if (idx == 0) {
+						res.data.map((item) => {
+							topVideo.push({
+								imageUrl: item.image_text,
+								videoUrl: item.video_text,
+								id: item.id,
+							});
+						});
+					} else if (idx == 1) {
+						res.data.map((item) => {
+							cePinginfo.push({
+								imageUrl: item.image_text,
+								id: item.id,
+								videoUrl: item.video_text,
+								name: item.name,
+								text: item.text,
+							});
+						});
+					} else if (idx == 2) {
+						res.data.map((item) => {
+							howVideoInfo.push({
+								id: item.id,
+								name: item.name,
+								introduction: {
+									imageUrl: item.image_text,
+									videoUrl: item.video_text,
+								},
+							});
+						});
+					}
+				});
+
+				this.introduction = topVideo[0];
+
+				this.cePinginfo = {
+					one: {
+						textcontent: {
+							title: cePinginfo[0].name,
+							text: cePinginfo[0].text,
+						},
+						introduction: {
+							imageUrl: cePinginfo[0].imageUrl,
+							videoUrl: cePinginfo[0].imageUrl,
+							id: cePinginfo[0].id,
+						},
+					},
+				};
+
+				this.howVideoInfo = howVideoInfo;
+
+
+
+			},
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	@import "../../../assets/styles/minix.scss";
+
+	.IndexPage {
+		width: 100%;
+
+		.banner {
+			@include fj(center);
+			width: 100%;
+			height: 700px;
+			background: rgb(196, 196, 196);
+			position: relative;
+
+			.logo {
+				width: 200px;
+				height: 200px;
+				position: absolute;
+				border-radius: 20px;
+				background: rgb(230, 125, 125);
+				left: 348px;
+				top: 580px;
+				z-index: 100;
+			}
+
+			.star {
+
+				position: absolute;
+				left: 585px;
+				top: 735px;
+				z-index: 100;
+				width: 50px;
+				height: 50px;
+				font-size: 36px;
+			}
+
+			.star2 {
+
+				position: absolute;
+				left: 585px;
+				top: 735px;
+				z-index: 100;
+				width: 50px;
+				height: 50px;
+				font-size: 36px;
+				color: #e67d7d;
+			}
+		}
+
+		.container1 {
+			@include ct1200;
+			padding: 80px;
+
+			display: flex;
+			flex-direction: column;
+
+			.title {
+				color: rgb(0, 0, 0);
+				font-size: 36px;
+				font-weight: 700;
+				line-height: 60px;
+				margin-top: 20px;
+
+			}
+
+			.content {
+
+				display: flex;
+				flex-direction: column;
+
+				.xiangmu {
+					margin: 20px 0;
+
+					.title {
+						color: rgb(0, 0, 0);
+						font-size: 36px;
+						font-weight: 700;
+						width: 100%;
+
+
+					}
+
+					.text {
+						color: rgb(0, 0, 0);
+						width: 100%;
+						font-size: 26px;
+						font-weight: 700;
+						line-height: 32px;
+						letter-spacing: 0px;
+						word-break: break-word;
+
+					}
+				}
+
+
+			}
+		}
+
+		.foot {
+			@include ct1200;
+			padding: 40px 0;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			flex-direction: column;
+
+			margin-top: 40px;
+
+			.foot-img {
+				width: 200px;
+				height: 200px;
+
+				img {
+					width: 100%;
+					height: 100%;
+					object-fit: cover;
+				}
+			}
+
+			.foot-text {
+				@include mt(20px);
+
+				color: rgb(0, 0, 0);
+				font-size: 26px;
+				font-weight: 700;
+				line-height: 30px;
+			}
+		}
+	}
+</style>
