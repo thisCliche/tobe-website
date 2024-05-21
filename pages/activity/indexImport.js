bd7f9bdbe3c6c4439315355dbd9af9cf67da5045
chululu <83503768@qq.com>
2024-05-21 20:48:36 +0800

    1111

---
+++ pages/activity/indexImport.js
@@ -0,0 +1,13 @@
+import modelBg from '@image/science/modelBg.jpg';
+import modelBg2 from '@image/science/modelBg2.jpg';
+import model1 from '@image/science/model1.png';
+import model2 from '@image/science/model2.png';
+import model3 from '@image/science/model3.png';
+import model4 from '@image/science/model4.png';
+import model5 from '@image/science/model5.png';
+import model6 from '@image/science/model6.png';
+import model7 from '@image/science/model7.png';
+import InfotroMask from '@image/scientific.png'
+
+let resourceImg = {modelBg,modelBg2,model1,model2,model3,model4,model5,model6,model7,InfotroMask}
+export default resourceImg;
